a660c4598f0e3b44d3506e33cee75cf9efb1a523
johnhoow <johnhoow14@gmail.com>
2014-11-21 01:28:53 +0800

    底部播放栏

--- gen/net/john/mplayer/R.java
+++ gen/net/john/mplayer/R.java
@@ -26,11 +26,11 @@ public final class R {
     public static final class drawable {
         public static final int actionbar_background=0x7f020000;
         public static final int actionbar_tab_indicator=0x7f020001;
-        public static final int back=0x7f020002;
-        public static final int forward=0x7f020003;
-        public static final int ic_action_search=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int play=0x7f020006;
+        public static final int ic_action_next=0x7f020002;
+        public static final int ic_action_play_over_video=0x7f020003;
+        public static final int ic_action_previous=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
         public static final int tab_selected_pressed=0x7f020007;
         public static final int tab_selected_unpressed=0x7f020008;
         public static final int tab_unselected_pressed=0x7f020009;

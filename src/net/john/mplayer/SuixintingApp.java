ff36f7e0626150bb6421519227d0aa22e044fb3f
johnhoow <johnhoow14@gmail.com>
2014-12-20 19:06:09 +0800

    ..

--- src/net/john/mplayer/SuixintingApp.java
+++ src/net/john/mplayer/SuixintingApp.java
@@ -14,7 +14,6 @@ public class SuixintingApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        
         initPath();
     }
     
---
+++ src/net/john/mplayer/service/MediaService.java
@@ -0,0 +1,19 @@
+package net.john.mplayer.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MediaService extends Service {
+
+    public MediaService() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
